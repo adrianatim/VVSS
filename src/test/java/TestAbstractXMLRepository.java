8f74bca306404350664f81a47b64d4a70fc86bf0
adrianatim <adriana_timis2017@yahoo.com>
2022-03-09 08:04:54 -0800

    Lab3_inClass

---
+++ src/test/java/TestAbstractXMLRepository.java
@@ -0,0 +1,26 @@
+import domain.Student;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import repository.StudentXMLRepo;
+
+public class TestAbstractXMLRepository {
+
+    @Test
+    public void test_saveStudent_expectNullPointerException() {
+        StudentXMLRepo studentXMLRepo = new StudentXMLRepo("src/main/resources/Studenti.xml");
+
+        NullPointerException exception = Assertions.assertThrows(NullPointerException.class, () -> {
+            studentXMLRepo.save(null);
+        });
+        Assertions.assertEquals("Cannot invoke \"repository.HasID.getID()\" because \"entity\" is null", exception.getMessage());
+    }
+
+    @Test
+    public void test_saveStudent_expectNothing() {
+        Student student = new Student("1", "Ana", 937, "anaAre@mere");
+        StudentXMLRepo studentXMLRepo = new StudentXMLRepo("src/main/resources/Studenti.xml");
+        Student studentToCompare = studentXMLRepo.save(student);
+
+        Assertions.assertEquals(student, studentToCompare);
+    }
+}
